8f9ab55a6468d0b0d16b999abdba149e22be5c1f
toshiki-akagawa <toshiki.akagawa.927@ctc-g.co.jp>
2022-06-15 06:47:50 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,5 +1,5 @@
 module "s3-bucket" {
-  source  = "app.terraform.io/YOURNAME-training/s3-bucket/aws"
+  source  = "app.terraform.io/Toshiki-training/s3-bucket/aws"
   version = "2.2.0"
   bucket_prefix = "PREFIX"
 }
